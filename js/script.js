896ac28eb3ebf10441d77bfd98ed8e872e4e7884
Kamalhosssain <131284109+Kamalhosssain@users.noreply.github.com>
2024-03-27 22:21:58 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//jq
+
+$(document).ready(function(){
+//counter up start
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+//mixitup
+
+var mixer = mixitup('#hello', {
+   
+    animation: {
+        duration: 300
+    }
+});
+
+
+
+
+});
+
